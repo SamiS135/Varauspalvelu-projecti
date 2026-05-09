b1a77b3fa097b1472b50c9fc86fade72a6ba1cf0
sahvenj <p87275@edu.sasky.fi>
2026-05-09 11:17:43 +0300

    testaas
    tais löytyy ongelma

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddOpenApiDocument(config =>
     config.Version = "v1";
 });
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
 app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
